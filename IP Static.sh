61877093dcfc5e8e5bf86ad16555ee6ecfe6a47e
DarkZ <146022304+RISHABH12005@users.noreply.github.com>
2025-05-04 00:51:12 +0530

    Update IP Static.sh

--- IP Static.sh
+++ IP Static.sh
@@ -9,9 +9,9 @@ static ip_address=192.168.1.100/24         # Set your desired static IP
 static routers=192.168.1.1                 # Your router's IP (Gateway)
 static domain_name_servers=192.168.1.1     # DNS server (can also use 8.8.8.8 for Google DNS)
 
-192.168.1.100 -> 192.168.1.1               # Replace IP
+# 192.168.1.100 -> 192.168.1.1               # Replace IP
 
-Ctrl+O -> Enter -> Ctrl + X                # Save & Exit
+# Ctrl+O -> Enter -> Ctrl + X                # Save & Exit
 
 sudo systemctl restart dhcpcd
 
