df896551a6ddf757b480cee7ed9f2d2326201ff4
DarkZ <146022304+RISHABH12005@users.noreply.github.com>
2025-06-28 01:48:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 import struct
 from fastapi.responses import StreamingResponse
-from moter import move
+from M import move
 from cam import generate_frames
-from sensor import sensor
+#from sensor import sensor
 import asyncio
 import cv2
-from fastapi import FastAPI, WebSocket
+from fastapi import FastAPI, WebSocket,Request
 from fastapi.middleware.cors import CORSMiddleware
-from ultralytics import YOLO
+#from ultralytics import YOLO
 import struct  
 
 app = FastAPI()
@@ -20,19 +20,21 @@ app.add_middleware(
     allow_headers=["*"],   
 )
 
+'''Load YOLO Model
 model_path = "./last.pt"
 model = YOLO(model_path)  
 model.eval()
-sensor()
+sensor()'''
 
 @app.websocket("/share")
 async def video_stream(websocket: WebSocket):
     await websocket.accept()
-    print("✅ Client Connected!")
+    print("Client Connected!")
+
     cap = cv2.VideoCapture(0)
     
     if not cap.isOpened():
-        print("🚨 Camera not found! Closing connection.")
+        print("Camera not found! Closing connection.")
         await websocket.close()
         return
     
@@ -40,7 +42,7 @@ async def video_stream(websocket: WebSocket):
         while True:
             ret, frame = cap.read()
             if not ret:
-                print("❌ No frame captured. Closing connection.")
+                print("No frame captured. Closing connection.")
                 break
 
             results = model.predict(frame)  
@@ -48,19 +50,23 @@ async def video_stream(websocket: WebSocket):
             _, buffer = cv2.imencode(".jpg", frame)
             frame_bytes = buffer.tobytes()
             frame_size = len(frame_bytes)
+
             header = struct.pack("<Q", frame_size)
             await websocket.send_bytes(header + frame_bytes)
-            print(f"📤 Sent Frame ({frame_size} bytes)")
+
+            print(f"Sent Frame ({frame_size} bytes)")
+
             await asyncio.sleep(0.033)
 
     except Exception as e:
-        print(f"⚠️ WebSocket Error: {e}")
+        print(f"WebSocket Error: {e}")
 
     finally:
         cap.release()
         if websocket.client_state.name != "DISCONNECTED":
             await websocket.close()
-        print("🔌 WebSocket Connection Closed")
+        print("WebSocket Connection Closed")
+
 
 @app.get("/")
 async def root():
@@ -69,6 +75,7 @@ async def root():
 @app.post("/")
 async def control_motor(request: Request):
     data = await request.json()
+    print(data,"data")
     direction = data.get("direction")
     if direction:
         move(direction)
